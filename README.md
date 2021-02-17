c08d8b9d08fb028971573037fbafe0c211aa5f63
Quentin Guidée <quentin.guidee@gmail.com>
2021-02-17 16:13:34 +0100

    [all] Init project with create-react-library@3.1.1

---
+++ README.md
@@ -0,0 +1,17 @@
+# mun-ui
+
+## Install
+
+```bash
+npm install --save mun-ui
+```
+
+or
+
+```bash
+yarn add mun-ui
+```
+
+## License
+
+MIT © [quentinguidee](https://github.com/quentinguidee)
---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'mun-ui'
+import 'mun-ui/dist/index.css'
+
+const App = () => {
+    return <ExampleComponent text='Create React Library Example 😄' />
+}
+
+export default App
