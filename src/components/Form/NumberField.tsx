c4e6b7b7b79ae70fee2a9648f03b8ec4f10d033d
Quentin Guidée <quentin.guidee@gmail.com>
2021-02-28 22:47:54 +0100

    [form] Reduce NumberField width

--- src/components/Form/NumberField.tsx
+++ src/components/Form/NumberField.tsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+/** @jsx jsx */
+import { jsx, css } from '@emotion/react'
+import { Component } from 'react'
 import { TextField, TextFieldProps } from './TextField'
 
 interface NumberFieldProps extends Omit<TextFieldProps, 'ref'> {}
 
 export class NumberField extends Component<NumberFieldProps> {
     render() {
-        return <TextField {...this.props} type='number' />
+        return (
+            <TextField
+                {...this.props}
+                css={css(`
+                    min-width: 100px;
+                    width: 100px;
+                `)}
+                type='number'
+            />
+        )
     }
 }
