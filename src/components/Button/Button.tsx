76adfba4ea8d1f6430a830088a8d55620e1d963c
Quentin Guidée <quentin.guidee@gmail.com>
2021-03-01 22:35:02 +0100

    [button] ButtonLink

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { Link } from 'react-router-dom'
 
 export const Button = styled.div`
     display: inline-block;
@@ -9,6 +10,22 @@ export const Button = styled.div`
     cursor: pointer;
     transition: all 0.1s;
     user-select: none;
+    &:hover {
+        background-color: ${(props) => props.theme.color.accent};
+        color: ${(props) => props.theme.color.text};
+    }
+`
+
+export const ButtonLink: any = styled(Link)`
+    display: inline-block;
+    padding: 8px 12px;
+    border-radius: 6px;
+    background-color: ${(props) => props.theme.color.secondaryBackground};
+    color: ${(props) => props.theme.color.accent};
+    cursor: pointer;
+    transition: all 0.1s;
+    user-select: none;
+    text-decoration: none;
     &:hover {
         background-color: ${(props) => props.theme.color.accent};
         color: ${(props) => props.theme.color.text};
